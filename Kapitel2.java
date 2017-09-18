3ae79b62a78777f8f077e8bc2899db099ba33ee9
Emil Edberg <edem16@nti.umea>
2017-09-18 11:10:22 +0200

    uppg 2.8

--- Kapitel2.java
+++ Kapitel2.java
@@ -1,17 +1,14 @@
 import javax.swing.*;
 /*
 	Emil Edberg
-	Uppgift 2.3
+	Uppgift 2.8
 	18/09-18
 */
 
 public class Kapitel2{
 	public static void main(String[] args){
-		char stort = '\u00D8';
-		char litet = '\u00F8';
-		char pi = '\u03C0';
-		JOptionPane.showMessageDialog(null, stort);
-		JOptionPane.showMessageDialog(null, litet);
-		JOptionPane.showMessageDialog(null, pi);
-}
+		String a = "Programspråk";
+		String b = a.charAt(1) + a.substring(5, 8) + a.charAt(5);
+		JOptionPane.showMessageDialog(null,b);
+	}
 }
