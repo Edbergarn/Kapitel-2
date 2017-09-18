e6becce9af83059948c3f944723f13a5d30d1286
Emil Edberg <edem16@nti.umea>
2017-09-18 10:45:37 +0200

    Uppg 2.3

--- Kapitel2.java
+++ Kapitel2.java
@@ -1,14 +1,15 @@
 import javax.swing.*;
-
+/*
+	Emil Edberg
+	Uppgift 2.3
+	18/09-18
+*/
 
 public class Kapitel2{
 	public static void main(String[] args){
-		String Name = "Emil Edberg";
-		String Adress = "Västanbäck 29";
-		String Number = "0703902222";
-		String Info = Name + "\n" + Adress + "\n"+ Number;
-		JOptionPane.showMessageDialog(null,Info);
-
-
+		String Firstname = JOptionPane.showInputDialog(null,"Ditt f" + '\u00F6' + "rnamn?" );
+		String Lastname = JOptionPane.showInputDialog(null, "Ditt efternamn?");
+		String Fullname = Firstname + " " + Lastname;
+		JOptionPane.showMessageDialog(null, "Du heter " + Fullname);
 	}
 }
