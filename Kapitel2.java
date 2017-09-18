bbd7a9a15e13bb355dd4860e680aac4808f6f71f
Emil Edberg <edem16@nti.umea>
2017-09-18 09:51:39 +0200

    uppg 2.1

---
+++ Kapitel2.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+
+public class Kapitel2{
+	public static void main(String[] args){
+		String Name = "Emil Edberg";
+		String Adress = "Västanbäck 29";
+		String Number = "0703902222";
+		String Info = Name + "\n" + Adress + "\n"+ Number;
+		JOptionPane.showMessageDialog(null,Info);
+
+
+	}
+}
