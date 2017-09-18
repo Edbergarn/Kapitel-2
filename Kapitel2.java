b05bc6a91b9b07283aa99f46e1394dae71c1e842
Emil Edberg <edem16@nti.umea>
2017-09-18 11:22:56 +0200

    uppg 2.9

--- Kapitel2.java
+++ Kapitel2.java
@@ -7,8 +7,12 @@ import javax.swing.*;
 
 public class Kapitel2{
 	public static void main(String[] args){
-		String a = "Programspråk";
-		String b = a.charAt(1) + a.substring(5, 8) + a.charAt(5);
+		String a, b;
+		a = "Erik Andersson 860314-2714";
+		int d1 = a.indexOf('1');
+		int d2 = a.indexOf('-');
+		int m1 = a.indexOf('0');
+		b = a.substring(d1,d2) + "/" + a.substring(m1, d1);
 		JOptionPane.showMessageDialog(null,b);
-	}
+	} 
 }
